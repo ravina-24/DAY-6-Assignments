7335d60e3a07c6296114228022c228ddcc6e89c4
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-11 23:30:42 +0530

    [Ravina] Add:Finding the fibonacci series upto input N

---
+++ UC_1_FibonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC_1_FibonacciSeries
+{
+    class UC_1_FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+
+            Console.Write("Enter the number of elements :    ");
+            Num = int.Parse(Console.ReadLine());
+
+
+
+            int f1 = 0, f2 = 1;
+            int fSeries;
+            int i;
+            Console.WriteLine("Fibonacci sequence : " + f1);
+            Console.WriteLine("Fibonacci sequence : " + f2);
+            for (i =2; i <Num; i++)
+            {
+                fSeries = f1 + f2;
+                Console.WriteLine("Fibonacci sequence : " + fSeries);
+                f1 = f2;
+                f2 = fSeries;
+
+                
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
