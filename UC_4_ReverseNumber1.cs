9df3a1c6b9d1fdf8483faa8114c7917eeeef5913
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-11 23:38:07 +0530

    [Ravina] Add: Showing the reverse of a number

---
+++ UC_4_ReverseNumber1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC_4_ReverseNumber
+{
+    class UC_4_ReverseNumber
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Console.Write("Enter the Number:  ");
+            Num = int.Parse(Console.ReadLine());
+
+
+           
+           int reverse = 0;
+            
+           while (Num>0)
+            {
+               int Output = (Num % 10);
+                reverse = reverse * 10 + Output;
+                Num = ( Num / 10);
+               
+
+            }
+            
+            
+            Console.WriteLine(+reverse+ ":is a reverse of a number");
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
