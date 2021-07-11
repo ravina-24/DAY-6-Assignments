d52676b4004a2e418894d045a9a1a56a55725eb7
ravina-24 <ravinamaudekar24@gmail.com>
2021-07-11 23:35:00 +0530

    [Ravina] Add:Checking whether the number is a prime number or not

---
+++ UC_3_PrimeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC_3_PrimeNumbers
+{
+    class UC_3_PrimeNumbers
+    {
+        static void Main(string[] args)
+        {
+
+
+            int Num;
+            Console.Write("Enter  the number :   ");
+            Num = int.Parse(Console.ReadLine());
+
+
+            int i = 2;
+            for (i = 2; i <= Num / 2; i++) 
+            {
+
+
+                int Check = (Num % i);
+
+                if (Check == 0)
+                {
+                    Console.WriteLine( +Num +    " :is not a  prime number");
+                    break;
+                }
+                else if (Check != 0)
+                {
+                    Console.WriteLine(+Num + ":is a prime number") ;
+                    break;
+                }
+                   
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
